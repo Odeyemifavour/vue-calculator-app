6c26312680926690b42d641788f2be47ade2961b
famero <odeyemifavour593@gmail.com>
2024-09-06 15:17:38 -0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="calculator-header">M! Calculator.</h1>
+    <Calculator/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue';
+  export default {
+    components: {
+      Calculator
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,52 @@
+<template>
+   <div class="calculator">
+   <div class="calculator-field">
+      <input type="text">
+   </div>
+   <div class="calculator-body">
+   <table> 
+    <tbody>
+      <tr>
+        <td class="button grey">C</td>
+        <td class="button grey">+/-</td>
+        <td class="button grey">%</td>
+        <td class="button gold">/</td>
+      </tr>
+     <tr>
+        <td class="button">7</td>
+        <td class="button">8</td>
+        <td class="button">9</td>
+        <td class="button gold">*</td>
+     </tr>
+     <tr>
+        <td class="button">4</td>
+        <td class="button">5</td>
+        <td class="button">6</td>
+        <td class="button gold">-</td>
+     </tr>
+     <tr>
+        <td class="button">1</td>
+        <td class="button">2</td>
+        <td class="button">3</td>
+        <td class="button gold">+</td>
+     </tr>
+     <tr>
+        <td colspan="2" class="button spanZero" >0</td>
+        <td class="button">.</td>
+        <td class="button gold">=</td>
+     </tr>
+    </tbody>
+   </table>
+   </div>
+   </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+<style lang="sass" scoped>
+
+  
+</style>
